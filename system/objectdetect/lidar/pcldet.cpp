aab70dcc4066646abae8aafff0be2a3f26c03d15
YunWu_QUB <vvface@gmail.com>
2019-01-24 14:26:39 +0000

    define the structure of object detection from image

---
+++ system/objectdetect/lidar/pcldet.cpp
@@ -0,0 +1,22 @@
+/*
+ * pcldet.cpp
+ *
+ *  Created on: 24 January 2019
+ *      Author: Yun Wu
+ *
+ *  This file contains the class for object detection
+ *  through the image from LiDAR. 
+ *
+ */
+
+class imgdet{
+    public:
+        // The public variables and functions:
+        // non-sensitive data and functions
+    private:
+        // The private variables and functions:
+        // internal temperal data and functions
+    protected:
+        // The protected variables and functions: 
+        // sensitive data and functions
+}
---
+++ system/objectdetect/radar/dopdet.cpp
@@ -0,0 +1,22 @@
+/*
+ * dopdet.cpp
+ *
+ *  Created on: 24 January 2019
+ *      Author: Yun Wu
+ *
+ *  This file contains the class for object detection
+ *  through the image from Radar. 
+ *
+ */
+
+class imgdet{
+    public:
+        // The public variables and functions:
+        // non-sensitive data and functions
+    private:
+        // The private variables and functions:
+        // internal temperal data and functions
+    protected:
+        // The protected variables and functions: 
+        // sensitive data and functions
+}
