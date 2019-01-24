7402f892733495eda901d365b34d1924525a2fb5
YunWu_QUB <vvface@gmail.com>
2019-01-24 15:24:36 +0000

    define the structure of object detection from image

--- system/objectdetect/ptzcam/imgdet.cpp
+++ system/objectdetect/ptzcam/imgdet.cpp
@@ -4,29 +4,6 @@
  *  Created on: 24 January 2019
  *      Author: Yun Wu
  *
- *  This file contains the class for object detection
- *  through the image from PTZ camera. 
- *
- *  The imgdet class is a wrapper of darknet calling all
- *  the necessary function calls for MUEAVI object detection. 
- *  The major steps of imgdet includes:
- *  1. extract ROI
- *     (we need to define the ROI)
- *  2. detect the object
- *     (we need to define the object)
- *  3. output object information 
- *     (we need to define the information format)
  */
 
-
-class imgdet{
-    public:
-        // The public variables and functions:
-        // non-sensitive data and functions
-    private:
-        // The private variables and functions:
-        // internal temperal data and functions
-    protected:
-        // The protected variables and functions: 
-        // sensitive data and functions
-}
+#include "imgdet.hpp"
